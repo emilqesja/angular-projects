4e43f7dfb84d717d02c40d9275c2cf7e83583ccd
Emil Qesja <emilqesja@outlook.com>
2022-02-10 12:51:24 +0100

    video-upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
+  isDragover = false;
+  file: File | null = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  storeFile($event: Event) {
+    this.isDragover = false;
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null; //If we find an undefined value we return null
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+    console.log(this.file);
   }
-
 }
